58dfcfd1859454a9b30508bea05d03040fb6e644
swbradshaw <scott@chillaxing.com>
2015-02-21 13:28:46 -0500

    Device List view working

--- app/src/main/java/com/hackathon/digitallifevoice/api/DigitalLifeViewConfiguration.java
+++ app/src/main/java/com/hackathon/digitallifevoice/api/DigitalLifeViewConfiguration.java
@@ -8,13 +8,18 @@ public class DigitalLifeViewConfiguration {
     public static ArrayList<String> editableDeviceList = new ArrayList<String>();
 
     static {
-        viewableDeviceList.add("contact-sensor");
-        viewableDeviceList.add("water-sensor");
+       // viewableDeviceList.add("contact-sensor");
+        //viewableDeviceList.add("water-sensor");
         viewableDeviceList.add("smart-plug");
         viewableDeviceList.add("door-lock");
 
-        editableDeviceList.add("smart-plug");
-        editableDeviceList.add("door-lock");
+        viewableDeviceList.add("camera");
+        viewableDeviceList.add("digital-life-controller");
+//
+//        editableDeviceList.add("smart-plug");
+//        editableDeviceList.add("door-lock");
+//        editableDeviceList.add("camera");
+//        editableDeviceList.add("digital-life-controller");
     }
 
 }
