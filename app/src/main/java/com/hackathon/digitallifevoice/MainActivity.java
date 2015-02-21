b8a32a9907cd227e712fab9d6ef4014b7971ff22
swbradshaw <scott@chillaxing.com>
2015-02-21 15:11:08 -0500

    Adding speech recognition to main screen

--- app/src/main/java/com/hackathon/digitallifevoice/MainActivity.java
+++ app/src/main/java/com/hackathon/digitallifevoice/MainActivity.java
@@ -2,13 +2,18 @@ package com.hackathon.digitallifevoice;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.Fragment;
+import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.preference.PreferenceManager;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.hackathon.digitallifevoice.api.DigitalLifeController;
 import com.hackathon.digitallifevoice.api.DigitalLifeDevice;
@@ -16,7 +21,9 @@ import com.hackathon.digitallifevoice.data.Action;
 import com.hackathon.digitallifevoice.data.DatabaseHandler;
 import com.hackathon.digitallifevoice.fragments.ActionsListFragment;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 /**
  * A list view that shows the current list of paired actions
@@ -24,23 +31,45 @@ import java.util.List;
 public class MainActivity extends BaseActivity {
 
 
-    int ADD_DEVICE =1;
-    int SETTINGS = 2;
-    int DEVICE_LIST = 3;
+    private final int ADD_DEVICE =1;
+    private final int SETTINGS = 2;
+    private final int DEVICE_LIST = 3;
+    private final int REQ_CODE_SPEECH_INPUT = 4;
 
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == ADD_DEVICE) {
-            if (resultCode == Activity.RESULT_OK) {
-                long actionId = data.getLongExtra("id", 0);
-                if (actionId != 0) {
-                    DatabaseHandler db = new DatabaseHandler(this);
-                    ActionsListFragment fragment = (ActionsListFragment)this.getFragmentManager().findFragmentById(R.id.fragment);
-                    fragment.addItem(db.getAction(actionId));
-                }
 
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
+
+                    //CharSequence[] results = data.getCharSequenceArrayExtra(RecognizerIntent.EXTRA_RESULTS);
+                    ArrayList<String> speechResult = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    CharSequence[] results = speechResult.toArray(new CharSequence[speechResult.size()]);
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setTitle("Select");
+                    builder.setItems(results, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            // the user clicked on colors[which]
+                        }
+                    });
+                    builder.show();
+                }
+                break;
+            }
+            case ADD_DEVICE: {
+                if (resultCode == Activity.RESULT_OK) {
+                    long actionId = data.getLongExtra("id", 0);
+                    if (actionId != 0) {
+                        DatabaseHandler db = new DatabaseHandler(this);
+                        ActionsListFragment fragment = (ActionsListFragment) this.getFragmentManager().findFragmentById(R.id.fragment);
+                        fragment.addItem(db.getAction(actionId));
+                    }
+                    break;
+                }
             }
 
         }
@@ -65,6 +94,26 @@ public class MainActivity extends BaseActivity {
 
 
 
+    /**
+     * Showing google speech input dialog
+     * */
+    private void promptSpeechInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 5);
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                getString(R.string.speech_prompt));
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(),
+                    getString(R.string.speech_not_supported),
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -77,6 +126,9 @@ public class MainActivity extends BaseActivity {
             Intent myIntent = new Intent(this, SettingsActivity.class);
             startActivityForResult(myIntent, this.SETTINGS);
             return true;
+        }
+        if (id == R.id.action_voicetest) {
+            promptSpeechInput();
         }
         if (id == R.id.action_devices) {
             Intent myIntent = new Intent(this, DeviceListActivity.class);
